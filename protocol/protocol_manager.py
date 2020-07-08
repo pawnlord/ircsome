25ae872e21eabb3cf4c63f77da316da99ec6c8de
pawnlord <scruffduff321@gmail.com>
2020-07-08 16:31:58 -0400

    Work on formatting recieved message Add private message quick option (@)

--- protocol/protocol_manager.py
+++ protocol/protocol_manager.py
@@ -11,6 +11,7 @@ class protocol_manager:
 
 
         self.channel = ""
+        self.last_channel = ""
     
         # try twice, sometimes takes that long
         for i in [0, 1]:
@@ -29,25 +30,34 @@ class protocol_manager:
         if len(msg) > 0:
             if self.channel == "":
                 if msg[:5].lower() == "!join" or msg[:4].lower() == "join" :
+                    self.last_channel = self.channel
                     self.channel = msg.split(' ')[1]
                 pass
             else:
                 if msg[:5].lower() == "!join":
+                    self.last_channel = self.channel
                     self.channel = msg[6:]
                 if msg[0] == '!':
                     msg = msg[1:]
+                elif msg[0] == '@':
+                    msg = "PRIVMSG " + msg[1:]
                 else:
                     msg = "PRIVMSG " + self.channel + " " + msg
-        print("sending message " + msg)
         self.s.send((msg + '\r\n').encode('utf-8'))
     
     def get_message(self, size):
         try:
             msg = self.s.recv(size).decode()
             if "PING" in msg:
-                print("PING FOUND")
                 return_msg = "PONG :" + msg.split(':')[1].split('\r')[0]
                 self.send_message(return_msg)
+            info = msg.split(':')[1]
+            if "448" in info:
+                self.channel = self.last_channel
+            user = ""
+            if '!' in msg and not '001' in msg:
+                user = msg.split('!')[0][1:]
+                msg = "<{}>: ".format(user) + msg.split(':')[2]
             print(msg, end='')
         except socket.timeout:
             pass
